b303a21409c255ac2320cdaac9ea302385f538de
anurag-grapecity <anurag.pathak@grapecity.com>
2024-09-10 11:49:52 +0530

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import "@mescius/wijmo.styles/wijmo.css";
   import { FlexGrid } from "@mescius/wijmo.grid";
   import { onMount } from "svelte";
+  import "@mescius/wijmo.styles/themes/wijmo.theme.material.css";
 
   var countries: Array<string> = "US,Germany,UK,Japan,Italy,Greece".split(",");
   var data: Array<{
